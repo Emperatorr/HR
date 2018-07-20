65c33033333102f4fc91445b621d4278ed416bdd
Zakui <zakuijules@gmail.com>
2018-07-20 13:22:25 +0000

    create login logout

---
+++ config/roles.py
@@ -0,0 +1,38 @@
+from rolepermissions.roles import AbstractUserRole
+
+class HR(AbstractUserRole):
+    available_permissions = {
+        'login': True,
+        'logout': True,
+        'update_profile': True,
+        'create_employer': True,
+        'create_department': True,
+        'create_position': True,
+        'affect_to_department': True,
+        'affect_to_position': True,
+        'make_user_a_manager': True,
+        'request_leave': True,
+        'approuve_all_leave': True,
+        'reject_all_leave': True,
+        'manage_leave_settings': True,
+        'view_all_leave_history': True,
+    }
+
+class Manager(AbstractUserRole):
+    available_permissions = {
+        'login': True,
+        'logout': True,
+        'update_profile': True,
+        'request_leave': True,
+        'approuve_leave': True,
+        'reject_leave': True,
+        'view_leave_history': True,
+    }
+
+class Employer(AbstractUserRole):
+    available_permissions = {
+        'login': True,
+        'logout': True,
+        'update_profile': True,
+        'request_leave': True,
+    }
